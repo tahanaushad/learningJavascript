130520282d7a9366e5aa3342b5f9875cafd67d22
tahanaushad <tahanaushad>
2024-01-23 20:56:06 +0500

    - UI has been changed

--- script.js
+++ script.js
@@ -1,39 +1,5 @@
-const products = [{
-    title: "Shirt",
-    price: 500,
-    description: "lorem",
-    image: "https://images.unsplash.com/photo-1705798261243-b6892fd6821f?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-},
-{
-    title: "Pant",
-    price: 400,
-    description: "lorem",
-    image: "https://images.unsplash.com/photo-1682685795579-37e731bfefab?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-},
-{
-    title: "Shoe",
-    price: 1000,
-    description: "lorem",
-    image: "https://images.unsplash.com/photo-1705338560918-5225147f34de?q=80&w=1664&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-},
-{
-    title: "Pen",
-    price: 70,
-    description: "lorem",
-    image: "https://images.unsplash.com/photo-1682687220640-9d3b11ca30e5?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-},
-{
-    title: "Book",
-    price: 100,
-    description: "lorem",
-    image: "https://images.unsplash.com/photo-1705779235006-4111cb1a889e?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-},
-{
-    title: "Pen",
-    price: 70,
-    description: "lorem",
-    image: "https://images.unsplash.com/photo-1682687220640-9d3b11ca30e5?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-}];
+
+
 
 function createProductCard(product) {
     //list of all the elements
@@ -80,7 +46,8 @@ cartNum.innerText = counter ;
 
 }
 
-function removeItem (){
+function removeItem() {
+
     if (counter > 0) {
     const cartNum = document.getElementById("cartNumber");
     counter = counter-1; 
